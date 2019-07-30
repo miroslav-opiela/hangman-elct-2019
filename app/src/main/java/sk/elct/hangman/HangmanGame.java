316685a354146623b7d22544ee1a2c28d51dcf09
miroslav opiela <miroslav.opiela@upjs.sk>
2019-07-30 15:52:07 +0200

    obesenec - priprava - nefungujuca verzia

---
+++ app/src/main/java/sk/elct/hangman/HangmanGame.java
@@ -0,0 +1,89 @@
+package sk.elct.hangman;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class HangmanGame {
+    // na zaciatku mam 6 pokusov
+    public static final int DEFAULT_ATTEMPTS_LEFT = 6;
+
+    // znak este neuhadnuteho pismena
+    private static final char UNGUESSED_CHAR = '_';
+
+    // predefinovane slova na hadanie
+    private List<String> words = Arrays.asList(
+            "hangman", "game", "hippopotamus",
+            "troll", "anteater", "banana"
+    );
+
+    // slovo ktore treba uhadnut
+    private String challengeWord;
+
+    // aktualny stav hry - odhalene a skryte znaky
+    private StringBuilder guessedCharacters;
+
+    // zostavajuci pocet pokusov
+    private int attemptsLeft = DEFAULT_ATTEMPTS_LEFT;
+
+    private Random random;
+
+    protected HangmanGame(Random random) {
+        this.random = random;
+
+        initializeRandomWord();
+        initializeUnguessedWord();
+    }
+
+    public HangmanGame() {
+        this(new Random());
+    }
+
+    private void initializeRandomWord() {
+        int randomIndex = random.nextInt(words.size());
+
+        challengeWord = words.get(randomIndex);
+    }
+
+    private void initializeUnguessedWord() {
+        guessedCharacters = new StringBuilder(challengeWord.length());
+        for (int i = 0; i < challengeWord.length(); i++) {
+            guessedCharacters.append(UNGUESSED_CHAR);
+        }
+    }
+
+    public boolean guess(char character) {
+        if (attemptsLeft <= 0) {
+            throw new IllegalStateException("No more guessing attempts left");
+        }
+
+        boolean letterIsFound = false;
+        for (int i = 0; i < challengeWord.length(); i++) {
+            if (challengeWord.charAt(i) == character) {
+                letterIsFound = true;
+                guessedCharacters.setCharAt(i, character);
+            }
+        }
+        if (!letterIsFound) {
+            attemptsLeft--;
+        }
+        return letterIsFound;
+    }
+
+    public boolean isWon() {
+        return guessedCharacters.toString().equals(challengeWord);
+    }
+
+    public CharSequence getGuessedCharacters() {
+        return guessedCharacters;
+    }
+
+    public String getChallengeWord() {
+        return challengeWord;
+    }
+
+    public int getAttemptsLeft() {
+        return attemptsLeft;
+    }
+
+}
--- app/src/main/java/sk/elct/hangman/MainActivity.java
+++ app/src/main/java/sk/elct/hangman/MainActivity.java
@@ -12,64 +12,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MediaPlayer mediaPlayer;
-
-    private static final String TAG = "Krava";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "on create");
-        mediaPlayer = MediaPlayer.create(this,R.raw.cow);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "on start");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "on resume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "on pause");
-        mediaPlayer.release();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "on stop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "on destroy");
-    }
-
-    public void rollDice(View view) {
-        Random r = new Random();
-        int value = r.nextInt(6) + 1;
-
-        TextView textView = findViewById(R.id.helloWorldTextView);
-        textView.setText("hodil si " + value);
-        if (value == 6) {
-            textView.setTextColor(Color.RED);
-        } else {
-            textView.setTextColor(Color.GREEN);
-        }
 
     }
 
-    public void playSound(View view) {
-        mediaPlayer.start();
-    }
+
 }
