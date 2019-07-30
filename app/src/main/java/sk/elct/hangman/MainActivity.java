5dc7c2252b02369da372ecfa1ac8b9c54293c4bf
miroslav opiela <miroslav.opiela@upjs.sk>
2019-07-30 15:15:19 +0200

    druhy projekt - prehravanie mp3 suboru, zivotny cyklus aktivity

--- app/src/main/java/sk/elct/hangman/MainActivity.java
+++ app/src/main/java/sk/elct/hangman/MainActivity.java
@@ -1,8 +1,10 @@
 package sk.elct.hangman;
 
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,10 +12,47 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
+
+    private static final String TAG = "Krava";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "on create");
+        mediaPlayer = MediaPlayer.create(this,R.raw.cow);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "on start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "on resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "on pause");
+        mediaPlayer.release();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "on stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "on destroy");
     }
 
     public void rollDice(View view) {
@@ -28,5 +67,9 @@ public class MainActivity extends AppCompatActivity {
             textView.setTextColor(Color.GREEN);
         }
 
+    }
+
+    public void playSound(View view) {
+        mediaPlayer.start();
     }
 }
